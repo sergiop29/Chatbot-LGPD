f97c6df8df1d00fcb8bceac58b273970552862fc
maellson <mqmaellson39@gmail.com>
2024-05-15 11:23:58 -0300

    correções de versionamento Maelson

--- utils/chatBot.py
+++ utils/chatBot.py
@@ -1,24 +1,30 @@
 from langchain_openai.embeddings import OpenAIEmbeddings
+# from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
 from langchain_openai.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
+
 load_dotenv()  # Isso carrega as variáveis de ambiente do arquivo .env
 
 
 def create_vectorstore(chunks):
     embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.load(texts=chunks, embedding=embeddings)
+    new_vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
 
+    vectorstore = FAISS.load_local('vectorstore', embeddings)
+    vectorstore.merge_from(new_vectorstore)
     return vectorstore
 
+
+# O none aqui deixa o artributo como opcional
 def create_conversation_chain(vectorstore=None):
     if (not vectorstore):
-            embeddings = OpenAIEmbeddings()
-            vectorstore = FAISS.load_local(
-                'vectorstore', embeddings=embeddings, allow_dangerous_deserialization=True)
+        embeddings = OpenAIEmbeddings()
+        vectorstore = FAISS.load_local(
+            'vectorstore', embeddings)
 
     llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0.7)
 
