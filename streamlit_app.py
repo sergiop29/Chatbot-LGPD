b5db59cecb7389c418b090d04f8597d620a22c6a
sergiop29 <sergiopradera29@gmail.com>
2024-05-23 14:17:43 -0300

    teste de chat, favor não julgar

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,8 +53,8 @@ def main():
 
     # ---------------- TESTE A PARTIR DAQUI ----------------
     # Manter histórico de chat entre sessões
-    # if "messages" not in st.session_state:
-    #     st.session_state.messages = []
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
     # for message in st.session_state.messages:
     #     with st.chat_message(message["role"]):
     #         st.markdown(message["content"])
