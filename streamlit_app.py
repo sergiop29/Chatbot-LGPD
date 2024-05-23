0f9b52e9894dc9c897121fdfb950fd4daa29f28c
sergiop29 <sergiopradera29@gmail.com>
2024-05-23 15:02:49 -0300

    Botão de modelos e prioridade sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,72 @@ def main():
     col7.empty()
     st.header(':green[Converse com um especialista em LGPD] 💬')
 
+    with st.sidebar:
+    # ------ INICIO HABILITAR O PROCESSAMENTO DE ARQUIVOS PDF ----#
+
+    # pdf_docs = st.file_uploader(
+    #     "Carregue os seus arquivos, em formato PDF, aqui", accept_multiple_files=True)
+    # print(type(pdf_docs))
+
+    # if st.button('Processar'):
+    #     all_files_text = text.process_file(pdf_docs)
+
+    #     chunks = text.create_text_chunks(all_files_text)
+
+    #     vectorstore = chatBot.create_vectorstore(chunks)
+    #     # print(vectorstore)
+    #     st.session_state.conversation = chatBot.create_conversation_chain(
+    #         vectorstore)
+
+    # _____ FIM HABILITAR O PROCESSAMENTO DE ARQUIVOS PDF ____#
+
+        st.header('Seu Chatbot pessoal treinado pela LGPDNOW! ', divider='green')
+        st.write("")
+        st.caption(""" <p style='text-align:justify'>
+        A Lei Geral de Proteção de Dados Pessoais (LGPD) foi promulgada em 2018 com o objetivo de garantir os direitos à liberdade, privacidade e personalidade dos indivíduos. Ela se aplica a qualquer pessoa, seja física ou jurídica, incluindo empresas e órgãos públicos.
+    <p style='text-align:justify'>
+    A LGPD define regras claras para a coleta, armazenamento, uso e compartilhamento de dados pessoais. Isso significa que você tem mais controle sobre suas informações e pode saber como elas estão sendo utilizadas.
+    <p style='text-align:justify'>
+    Com a LGPD em vigor desde 2020, empresas e órgãos que não se adequarem à lei podem ser punidos com multas.
+        </p>""", unsafe_allow_html=True)
+        st.markdown("")
+
+        st.subheader('Modelos de Chatbot')
+        selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
+        if selected_model == 'Chat GPT 3.5':
+            llm = ''
+        elif selected_model == 'Llama2 13B':
+            llm = ''
+
+        st.markdown("")
+        st.subheader('Seus arquivos')
+        with st.expander("Legislação utilizada no modelo: "):
+            st.write("LEI No 13.709, DE 14 DE AGOSTO DE 2018")
+            # st.write("")
+            # st.write("")
+            # st.write("")
+            # st.write("")
+
+        # Botão de "Clear Chat"
+        st.markdown("")
+        def clear_chat_history():
+            st.session_state.messages = []
+            # response = ""
+            # prompt = ""
+        st.sidebar.button('Limpar Chat', on_click=clear_chat_history)
+        st.divider()
+
+        # Footer do sidebar
+        st.caption("<p style='text-align:center'> Made by LGPDNOW </p>",
+                    unsafe_allow_html=True)
+        col1, col2, col3, col4, col5 = st.columns(5)
+        col1.empty()
+        col2.empty()
+        col3.image('utils/lgpd_logo_verde.png', width=40)
+        col4.empty()
+        col5.empty()
+
+#Página principal
     # Manter histórico de chat entre sessões
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -42,62 +108,6 @@ def main():
             st.markdown(response)
         st.session_state.messages.append({"role": "assistant", "content": response})
 
-    with st.sidebar:
-        st.subheader('Seus arquivos')
-
-        # ------ INICIO HABILITAR O PROCESSAMENTO DE ARQUIVOS PDF ----#
-
-        # pdf_docs = st.file_uploader(
-        #     "Carregue os seus arquivos, em formato PDF, aqui", accept_multiple_files=True)
-        # print(type(pdf_docs))
-
-        # if st.button('Processar'):
-        #     all_files_text = text.process_file(pdf_docs)
-
-        #     chunks = text.create_text_chunks(all_files_text)
-
-        #     vectorstore = chatBot.create_vectorstore(chunks)
-        #     # print(vectorstore)
-        #     st.session_state.conversation = chatBot.create_conversation_chain(
-        #         vectorstore)
-
-        # _____ FIM HABILITAR O PROCESSAMENTO DE ARQUIVOS PDF ____#
-
-        st.header('Seu Chatbot pessoal treinado pela LGPDNOW! ', divider='green')
-        st.write("")
-        st.caption(""" <p style='text-align:justify'>
-        A Lei Geral de Proteção de Dados Pessoais (LGPD) foi promulgada em 2018 com o objetivo de garantir os direitos à liberdade, privacidade e personalidade dos indivíduos. Ela se aplica a qualquer pessoa, seja física ou jurídica, incluindo empresas e órgãos públicos.
-<p style='text-align:justify'>
-A LGPD define regras claras para a coleta, armazenamento, uso e compartilhamento de dados pessoais. Isso significa que você tem mais controle sobre suas informações e pode saber como elas estão sendo utilizadas.
-<p style='text-align:justify'>
-Com a LGPD em vigor desde 2020, empresas e órgãos que não se adequarem à lei podem ser punidos com multas.
-        </p>""", unsafe_allow_html=True)
-        st.markdown("")
-
-        with st.expander("Legislação utilizada no modelo: "):
-            st.write("LEI No 13.709, DE 14 DE AGOSTO DE 2018")
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-        # Botão de "Clear Chat"
-        st.markdown("")
-        def clear_chat_history():
-            st.session_state.messages = []
-            # response = ""
-            # prompt = ""
-        st.sidebar.button('Limpar Chat', on_click=clear_chat_history)
-        st.divider()
-        
-        # Footer do sidebar
-        st.caption("<p style='text-align:center'> Made by LGPDNOW </p>",
-                   unsafe_allow_html=True)
-        col1, col2, col3, col4, col5 = st.columns(5)
-        col1.empty()
-        col2.empty()
-        col3.image('utils/lgpd_logo_verde.png', width=40)
-        col4.empty()
-        col5.empty()
 
 if __name__ == '__main__':
 
